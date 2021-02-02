c687d5c9fe90ee9e3f61e74ab0777a6dee1b2151
edison chang <edison.chang@esi-tech.net>
2021-02-02 17:07:03 +0800

    Day2_Builder

---
+++ Day2/Builder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GOF_DesignPattern.Day2;
+
+namespace GOF_DesignPattern.Day2
+{
+    interface Builder
+    {
+        public void reset();
+        public void setDataSource(string s);
+        public void setUserID(string s);
+        public void setPassword(string s);
+        public void setInitialCatalog(string s);
+    }
+    
+    public class MssqlDbBuilder : Builder
+    {
+        private MSsql DB;
+        public MssqlDbBuilder()
+        {
+            this.reset();
+        }
+        public void reset()
+        {
+            this.DB = new MSsql();
+        }
+        public void setDataSource(string s)
+        {
+            this.DB.DataSource = s;
+        }
+
+        public void setUserID(string s)
+        {
+            this.DB.UserID = s;
+        }
+
+        public void setPassword(string s)
+        {
+            this.DB.Password = s;
+        }
+
+        public void setInitialCatalog(string s)
+        {
+            this.DB.InitialCatalog = s;
+        }
+
+        public string getConnectString()
+        {
+
+            return "Data Source="+ this.DB.DataSource + ";Initial Catalog="+this.DB.InitialCatalog+";User ID="+ this.DB.UserID + ";Password="+this.DB.Password;
+        }
+
+    }
+}
---
+++ Day2/Director.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GOF_DesignPattern.Day2;
+
+namespace GOF_DesignPattern.Day2
+{
+    // 實作Demo
+    // 用director將product建立出來，並且將裡面實作方法封裝，Client只要知道呼叫即可取得product
+    /*      Director dr = new Director();
+            MssqlDbBuilder br = new MssqlDbBuilder();
+            dr.constructSportsCar(br);
+            Console.WriteLine(br.getConnectString());
+    // 建立部分組件時，直接用Builder將product建立出來
+            br.setDataSource("ming-stock.database.windows.net_u");
+            br.setInitialCatalog("stock_AllPrice_u");
+            br.setUserID("adming_u");
+            br.setPassword("Sao015207_u");
+            Console.WriteLine(br.getConnectString());*/
+    class Director
+    {
+        // 用Director實際去安排該複雜物件要有甚麼部分需要建立
+        public void constructSportsCar(MssqlDbBuilder builder)
+        {
+            builder.reset();
+            builder.setDataSource("ming-stock.database.windows.net");
+            builder.setInitialCatalog("stock_AllPrice");
+            builder.setUserID("adming");
+            builder.setPassword("Sao015207");
+        }
+
+    }
+}
